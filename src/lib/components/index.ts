ae82aa9af24aef53457280700920648382ef8757
Ricardo Soares <soaresricaventura@gmail.com>
2025-11-12 11:15:55 +0000

    chore(exports): export Dropdown from components index

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button.svelte';
 export { default as IconButton } from './IconButton.svelte';
 export { default as TextInput } from './TextInput.svelte';
 export { default as Toggle } from './Toggle.svelte';
+export { default as Dropdown } from './Dropdown.svelte';
