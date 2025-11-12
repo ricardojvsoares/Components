8b77bfd2385665eb86ea814e566bf6c93cfe658d
Ricardo Soares <soaresricaventura@gmail.com>
2025-11-12 12:59:54 +0000

    chore: remove favicon import and link from layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
 	import NavBar from '$lib/components/NavBar.svelte';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <NavBar title="Component gallery" links={[{ href: '/', label: 'Home' }]}></NavBar>
 
